a44a56e3727778c25da7f344ae2fd9a6d86ca6d5
Ana Nikolova <ani.nik19@gmail.com>
2019-08-25 09:45:06 +0200

    created search box component

--- starter/src/App.js
+++ starter/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import CardList from "./components/card-list/CardList";
+import SearchBox from "./components/search-box/SearchBox";
 import "./App.css";
 
 class App extends Component {
@@ -10,6 +11,12 @@ class App extends Component {
       searchField: ""
     };
   }
+
+  handleChange = e => {
+    this.setState({
+      searchField: e.target.value
+    });
+  };
 
   componentDidMount() {
     fetch("https://jsonplaceholder.typicode.com/users")
@@ -24,10 +31,9 @@ class App extends Component {
     );
     return (
       <div className="App">
-        <input
-          type="search"
+        <SearchBox
           placeholder="search monster"
-          onChange={e => this.setState({ searchField: e.target.value })}
+          handleChange={this.handleChange}
         />
         <CardList monsters={filteredMonsters} />
       </div>
